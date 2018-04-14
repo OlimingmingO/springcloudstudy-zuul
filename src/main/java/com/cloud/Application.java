0b427c555dd77c85f3e7e234eb9a0cf81499793f
lizhentaosky <lizhentaosky@163.com>
2018-04-14 09:44:46 +0800

    阿萨德

---
+++ src/main/java/com/cloud/Application.java
@@ -0,0 +1,20 @@
+package com.cloud;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+
+/**
+ * Created by Oliverlee on 2018/4/2.
+ */
+@EnableZuulProxy
+@SpringCloudApplication
+public class Application {
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(Application.class).web(true).run(args);
+    }
+    @Bean
+    public AccessFilter accessFilter() {
+        return new AccessFilter();
+    }
+}
